ad005c3c7510d5f58cd089ba57ebb19a662e8fc9
jenish <sabhadiyajenish83@gmail.com>
2024-05-27 18:03:48 +0530

    dfdd

--- src/utils/authUtils.js
+++ src/utils/authUtils.js
@@ -1,12 +1,12 @@
 import jwt from "jsonwebtoken";
 
 const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET; // Replace with your actual secret key
-const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET; // Replace with your actual secret key
+const JWT_REFRESH_SECRET = process.env.ACCESS_TOKEN_EXPIRE; // Replace with your actual secret key
 
 export function generateAccessJWT(payload) {
   console.log("payload", payload);
   return jwt.sign(payload, JWT_ACCESS_SECRET, {
-    expiresIn: JWT_REFRESH_SECRET,
+    // expiresIn: JWT_REFRESH_SECRET,
   }); // Token expires in 1 day
 }
 
